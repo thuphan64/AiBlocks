ac0dc8c02a748a2bdfa876567a881028d5656722
aiblocks <41142785+aiblocks@users.noreply.github.com>
2018-07-14 22:43:31 +0000

    Add files via upload

---
+++ core/helper_test.go
@@ -0,0 +1,76 @@
+// Copyright 2014 The go-aiblocks Authors
+// This file is part of the go-aiblocks library.
+//
+// The go-aiblocks library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-aiblocks library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-aiblocks library. If not, see <http://www.gnu.org/licenses/>.
+
+package core
+
+import (
+	"container/list"
+
+	"github.com/aiblocksproject/go-aiblocks/core/types"
+	"github.com/aiblocksproject/go-aiblocks/ethdb"
+	"github.com/aiblocksproject/go-aiblocks/event"
+)
+
+// Implement our EthTest Manager
+type TestManager struct {
+	// stateManager *StateManager
+	eventMux *event.TypeMux
+
+	db         ethdb.Database
+	txPool     *TxPool
+	blockChain *BlockChain
+	Blocks     []*types.Block
+}
+
+func (s *TestManager) IsListening() bool {
+	return false
+}
+
+func (s *TestManager) IsMining() bool {
+	return false
+}
+
+func (s *TestManager) PeerCount() int {
+	return 0
+}
+
+func (s *TestManager) Peers() *list.List {
+	return list.New()
+}
+
+func (s *TestManager) BlockChain() *BlockChain {
+	return s.blockChain
+}
+
+func (tm *TestManager) TxPool() *TxPool {
+	return tm.txPool
+}
+
+// func (tm *TestManager) StateManager() *StateManager {
+// 	return tm.stateManager
+// }
+
+func (tm *TestManager) EventMux() *event.TypeMux {
+	return tm.eventMux
+}
+
+// func (tm *TestManager) KeyManager() *crypto.KeyManager {
+// 	return nil
+// }
+
+func (tm *TestManager) Db() ethdb.Database {
+	return tm.db
+}
